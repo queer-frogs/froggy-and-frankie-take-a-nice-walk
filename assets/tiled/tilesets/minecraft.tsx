97a04c0498b31e29014be4daed994361820ef19c
Page-MS <pagems@proton.me>
2023-10-14 15:09:25 +0200

    just some assets

---
+++ assets/tiled/tilesets/minecraft.tsx
@@ -0,0 +1,403 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="minecraft" tilewidth="16" tileheight="16" tilecount="133" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/acacia_door_bottom.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/acacia_door_top.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/acacia_log.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/acacia_log_top.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/acacia_planks.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/acacia_trapdoor.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/amethyst_block.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/ancient_debris_side.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/ancient_debris_top.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/andesite.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/anvil.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/azalea_top.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_block.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_block_top.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_door_bottom.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_door_top.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_fence.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_fence_gate.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_fence_gate_particle.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_fence_particle.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_mosaic.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_planks.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_stalk.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bamboo_trapdoor.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/barrel_bottom.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/barrel_side.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/barrel_top.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/barrel_top_open.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/basalt_side.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/basalt_top.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/beacon.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bedrock.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bee_nest_bottom.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bee_nest_front.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bee_nest_front_honey.png"/>
+ </tile>
+ <tile id="35">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bee_nest_side.png"/>
+ </tile>
+ <tile id="36">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bee_nest_top.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/beehive_end.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/beehive_front.png"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/beehive_front_honey.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/beehive_side.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/birch_door_bottom.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/birch_door_top.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/birch_log.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/birch_log_top.png"/>
+ </tile>
+ <tile id="45">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/birch_planks.png"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/birch_trapdoor.png"/>
+ </tile>
+ <tile id="47">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/black_concrete.png"/>
+ </tile>
+ <tile id="48">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/black_concrete_powder.png"/>
+ </tile>
+ <tile id="49">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/black_glazed_terracotta.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/black_shulker_box.png"/>
+ </tile>
+ <tile id="51">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/black_terracotta.png"/>
+ </tile>
+ <tile id="52">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/black_wool.png"/>
+ </tile>
+ <tile id="53">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blackstone.png"/>
+ </tile>
+ <tile id="54">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blackstone_top.png"/>
+ </tile>
+ <tile id="55">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_concrete.png"/>
+ </tile>
+ <tile id="56">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_concrete_powder.png"/>
+ </tile>
+ <tile id="57">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_glazed_terracotta.png"/>
+ </tile>
+ <tile id="58">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_ice.png"/>
+ </tile>
+ <tile id="59">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_shulker_box.png"/>
+ </tile>
+ <tile id="60">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_terracotta.png"/>
+ </tile>
+ <tile id="61">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/blue_wool.png"/>
+ </tile>
+ <tile id="62">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bone_block_side.png"/>
+ </tile>
+ <tile id="63">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bone_block_top.png"/>
+ </tile>
+ <tile id="64">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bookshelf.png"/>
+ </tile>
+ <tile id="65">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brain_coral_block.png"/>
+ </tile>
+ <tile id="66">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brewing_stand_base.png"/>
+ </tile>
+ <tile id="67">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bricks.png"/>
+ </tile>
+ <tile id="68">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brown_concrete.png"/>
+ </tile>
+ <tile id="69">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brown_concrete_powder.png"/>
+ </tile>
+ <tile id="70">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brown_glazed_terracotta.png"/>
+ </tile>
+ <tile id="71">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brown_shulker_box.png"/>
+ </tile>
+ <tile id="72">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brown_terracotta.png"/>
+ </tile>
+ <tile id="73">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/brown_wool.png"/>
+ </tile>
+ <tile id="74">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bubble_coral.png"/>
+ </tile>
+ <tile id="75">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/bubble_coral_block.png"/>
+ </tile>
+ <tile id="76">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/budding_amethyst.png"/>
+ </tile>
+ <tile id="77">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cactus_bottom.png"/>
+ </tile>
+ <tile id="78">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cactus_side.png"/>
+ </tile>
+ <tile id="79">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cactus_top.png"/>
+ </tile>
+ <tile id="80">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cake_bottom.png"/>
+ </tile>
+ <tile id="81">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cake_top.png"/>
+ </tile>
+ <tile id="82">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/calcite.png"/>
+ </tile>
+ <tile id="83">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cartography_table_side1.png"/>
+ </tile>
+ <tile id="84">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cartography_table_side2.png"/>
+ </tile>
+ <tile id="85">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cartography_table_side3.png"/>
+ </tile>
+ <tile id="86">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cartography_table_top.png"/>
+ </tile>
+ <tile id="87">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/carved_pumpkin.png"/>
+ </tile>
+ <tile id="88">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cauldron_bottom.png"/>
+ </tile>
+ <tile id="89">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cauldron_inner.png"/>
+ </tile>
+ <tile id="90">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cauldron_side.png"/>
+ </tile>
+ <tile id="91">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cauldron_top.png"/>
+ </tile>
+ <tile id="92">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chipped_anvil_top.png"/>
+ </tile>
+ <tile id="93">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_bookshelf_empty.png"/>
+ </tile>
+ <tile id="94">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_bookshelf_occupied.png"/>
+ </tile>
+ <tile id="95">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_bookshelf_side.png"/>
+ </tile>
+ <tile id="96">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_bookshelf_top.png"/>
+ </tile>
+ <tile id="97">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_deepslate.png"/>
+ </tile>
+ <tile id="98">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_nether_bricks.png"/>
+ </tile>
+ <tile id="99">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_polished_blackstone.png"/>
+ </tile>
+ <tile id="100">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_quartz_block.png"/>
+ </tile>
+ <tile id="101">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_quartz_block_top.png"/>
+ </tile>
+ <tile id="102">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_red_sandstone.png"/>
+ </tile>
+ <tile id="103">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_sandstone.png"/>
+ </tile>
+ <tile id="104">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/chiseled_stone_bricks.png"/>
+ </tile>
+ <tile id="105">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/clay.png"/>
+ </tile>
+ <tile id="106">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/coal_block.png"/>
+ </tile>
+ <tile id="107">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/coal_ore.png"/>
+ </tile>
+ <tile id="108">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/coarse_dirt.png"/>
+ </tile>
+ <tile id="109">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cobbled_deepslate.png"/>
+ </tile>
+ <tile id="110">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cobblestone.png"/>
+ </tile>
+ <tile id="111">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/comparator.png"/>
+ </tile>
+ <tile id="112">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/comparator_on.png"/>
+ </tile>
+ <tile id="113">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/composter_bottom.png"/>
+ </tile>
+ <tile id="114">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/composter_compost.png"/>
+ </tile>
+ <tile id="115">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/composter_ready.png"/>
+ </tile>
+ <tile id="116">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/composter_side.png"/>
+ </tile>
+ <tile id="117">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/composter_top.png"/>
+ </tile>
+ <tile id="118">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/conduit.png"/>
+ </tile>
+ <tile id="119">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/copper_block.png"/>
+ </tile>
+ <tile id="120">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/copper_ore.png"/>
+ </tile>
+ <tile id="121">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cracked_deepslate_bricks.png"/>
+ </tile>
+ <tile id="122">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cracked_deepslate_tiles.png"/>
+ </tile>
+ <tile id="123">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cracked_nether_bricks.png"/>
+ </tile>
+ <tile id="124">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cracked_polished_blackstone_bricks.png"/>
+ </tile>
+ <tile id="125">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/cracked_stone_bricks.png"/>
+ </tile>
+ <tile id="126">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crafting_table_front.png"/>
+ </tile>
+ <tile id="127">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crafting_table_side.png"/>
+ </tile>
+ <tile id="128">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crafting_table_top.png"/>
+ </tile>
+ <tile id="129">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crimson_door_bottom.png"/>
+ </tile>
+ <tile id="130">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crimson_door_top.png"/>
+ </tile>
+ <tile id="131">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crimson_nylium.png"/>
+ </tile>
+ <tile id="132">
+  <image width="16" height="16" source="../tiles/Minecraft tiles/crimson_nylium_side.png"/>
+ </tile>
+</tileset>
