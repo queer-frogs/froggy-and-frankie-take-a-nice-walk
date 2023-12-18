454f7f12bc67da6ada4e9db3f195ef1b8f391daa
Page-MS <pagems@proton.me>
2023-12-18 11:50:24 +0100

    adding he scaled levels (not implemented)

---
+++ assets/tiled/tilemaps/original_sprites.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="original_sprites" tilewidth="32" tileheight="64" tilecount="29" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Bois.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Bois2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Brique.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="64" source="../../../../../../../../Downloads/Character.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../../../../../../../../Downloads/fleur bleu-1.png(1).png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../../../../../../../../Downloads/fleur rose-1.png(1).png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../../../../../../../../Downloads/herbe début-1.png(1).png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../../../../../../../../Downloads/herbe rose-1.png(1).png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Parchemin.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Red sky-1.png(1).png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../../../../../../../../Downloads/terre début-1.png(1).png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../../../../../../../../Downloads/terre rose-1.png(1).png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Terre.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Vitre.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Water.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Water2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../../../../../../../Downloads/Yellow -1.png(1).png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../../../../../../../../Downloads/fleur rose-1.png.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../../../../../../../../Downloads/fleur bleu-1.png.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../../../../../../../../Downloads/herbe rose-1.png.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../../../../../../../../Downloads/terre rose-1.png.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../../../../../../../../Downloads/Yellow -1.png.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Red sky-1.png.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="../../../../../../../../Downloads/herbe début-1.png.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="../../../../../../../../Downloads/terre début-1.png.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="../../../../../../../../Downloads/feuillage rose 2-1.png.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="../../../../../../../../Downloads/feuillage rose 1-1.png.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="../../../../../../../../Downloads/Bois clair-1.png.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="../../../../../../../../Downloads/New Piskel-1.png.png"/>
+ </tile>
+</tileset>
