e966b5cb1db06449a38499d2f291cbfe0b7e8e21
Lauranne Fossat <laurannefossat10@gmail.com>
2023-12-29 20:03:15 +0100

    ajout texture menu jeu et texture pour la fonction help

--- main_menu.py
+++ main_menu.py
@@ -8,8 +8,11 @@ class MenuView(arcade.View):
         self.manager = gui.UIManager()
         self.game_view = game_view
 
-        resume_button = gui.UIFlatButton(text="Resume", width=150)
-        exit_button = gui.UIFlatButton(text="Exit", width=150)
+        resume = arcade.load_texture("assets/tiled/tiles/own/Play.png")
+        resume_button = gui.UITextureButton(texture=resume, scale=4)
+
+        sortie = arcade.load_texture("assets/tiled/tiles/own/Exit.png")
+        exit_button = gui.UITextureButton(texture=sortie, scale=4)
 
         # Initialise a grid in which widgets can be arranged.
         self.box = gui.UIBoxLayout(x=100, y=100, vertical=True, children=[resume_button, exit_button])
@@ -49,10 +52,20 @@ class HelpView(arcade.View):
         self.manager = gui.UIManager()
         self.menu_view = menu_view
 
-        retour_button = gui.UIFlatButton(text="Retour", width=150)
+        retour = arcade.load_texture("assets/tiled/tiles/own/Retour.png")
+        retour_button = gui.UITextureButton(texture=retour, scale=2)
         self.box = gui.UIBoxLayout(x=100, y=100, vertical=True, children=[retour_button])
 
-        self.manager.add(gui.UIAnchorWidget(anchor_x='center', anchor_y='center', child=self.box))
+        self.manager.add(gui.UIAnchorWidget(anchor_x='right', anchor_y='bottom', child=self.box))
+
+        self.scene = arcade.Scene()
+        image_book = "assets/tiled/tiles/own/Book2.png"
+        self.book = arcade.Sprite(image_book)
+        self.book.scale = 2.3
+        self.book.center_x = 500
+        self.book.center_y = 250
+        self.scene.add_sprite("Book", self.book)
+
 
         @retour_button.event("on_click")
         def on_click_retour_button(event):
@@ -65,11 +78,13 @@ class HelpView(arcade.View):
     def on_show_view(self):
         """ This is run once when we switch to this view """
         # Makes the background darker
-        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+        arcade.set_background_color(arcade.color.ANTIQUE_WHITE)
+
         self.manager.enable()
 
     def on_draw(self):
         """ Render the screen. """
         # Clear the screen
         self.clear()
+        self.scene.draw()
         self.manager.draw()
