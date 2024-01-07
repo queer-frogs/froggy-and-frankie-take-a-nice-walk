9c47ac1164e56850ee949a05e85f8e20086cf293
Maxence Vahedi <maxence.vahedi@gmail.com>
2024-01-07 22:14:13 +0100

    tileset decoration

---
+++ assets/tiled/tilesets/decorations.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="decorations" tilewidth="16" tileheight="16" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="5">
+  <image width="16" height="16" source="../tiles/own/clouds/cloud_1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../tiles/own/clouds/cloud_2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../tiles/own/clouds/cloud_3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../tiles/own/clouds/cloud_4.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../tiles/own/clouds/cloud_5.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../tiles/own/haricot/haricot_0.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../tiles/own/haricot/haricot_1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../tiles/own/haricot/haricot_2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../tiles/own/haricot/haricot_3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../tiles/own/haricot/haricot_4.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_0.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_3.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_4.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_5.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_6.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_7.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="../tiles/own/troncs/tronc_8.png"/>
+ </tile>
+</tileset>
