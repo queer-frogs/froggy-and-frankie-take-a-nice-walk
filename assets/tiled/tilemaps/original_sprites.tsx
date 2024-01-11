a453705cc5485ecdff222ab51993947bc1f796ce
Page-MS <pagems@proton.me>
2024-01-11 16:39:49 +0100

    100

--- assets/tiled/tilemaps/original_sprites.tsx
+++ assets/tiled/tilemaps/original_sprites.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="original_sprites" tilewidth="64" tileheight="64" tilecount="49" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="original_sprites" tilewidth="64" tileheight="64" tilecount="50" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="35">
   <image width="16" height="16" source="../../backgrounds/Bois2.png"/>
@@ -147,5 +147,8 @@
  </tile>
  <tile id="86">
   <image width="64" height="64" source="../../backgrounds/sprite_05.png"/>
+ </tile>
+ <tile id="87">
+  <image width="64" height="64" source="../../backgrounds/100.png"/>
  </tile>
 </tileset>
